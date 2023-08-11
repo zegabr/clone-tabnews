f62015f7384f47ba114f7d72eed1c3820fa9d82b
zegabr <jjgabrielsp@gmail.com>
2023-08-10 22:40:24 -0300

    core: Add under construction page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
+import React from 'react';
+import Image from 'next/image';
+import under_construction_gif from '../gifs/under_construction_1.gif'
+
 function Home() {
-  return <h1> oi peste ;-) </h1>;
+  return (
+    <div>
+      <h1>Under Construction</h1>
+      <Image src={under_construction_gif} alt="Loading..." />
+    </div>
+  );
 }
 
 export default Home;
