f7479c9185962103df5ca1059c17868195684413
zegabr <jjgabrielsp@gmail.com>
2023-10-21 22:54:43 -0300

    add database.js and .env
    Signed-off-by: zegabr <jjgabrielsp@gmail.com>

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,9 @@
-function status(request, response){
- response.status(200).json({"opa": "this is a status message"})
+import database from "../../../../infra/database.js"
+
+async function status(request, response) {
+  const result = await database.query('SELECT 1 + 1;')
+  console.log(result)
+  response.status(200).json({ "opa": "this is a status message" })
 }
 
 export default status;
