9d0c5a30e9bc71fbfb8a54bea46fc4ca76412b8a
zegabr <jjgabrielsp@gmail.com>
2024-01-07 20:23:16 -0300

    add updated_at to status response
    Signed-off-by: zegabr <jjgabrielsp@gmail.com>

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,10 @@
 import database from "infra/database.js"
 
 async function status(request, response) {
-  const result = await database.query('SELECT 1 + 1;')
-  console.log(result)
-  response.status(200).json({ "opa": "this is a status message" })
+  const updatedAt = new Date().toISOString();
+  response.status(200).json({
+    updated_at: updatedAt
+  })
 }
 
 export default status;
